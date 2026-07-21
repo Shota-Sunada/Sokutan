0504005d05eea75fce7f571b77b018497b7b6d5d
Shota-Sunada <shota.sunada.dev@gmail.com>
2026-07-21 22:45:08 +0900

    update ui

--- src/App.tsx
+++ src/App.tsx
@@ -196,53 +196,54 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      {/* <p className="text-3xl my-2">CROWN & 速単 音声プレイヤー</p> */}
-      <p className="text-3xl my-2">英語教材 音声プレイヤー</p>
-      <label className="text-lg my-2">
-        <span className="mr-2">教材を選択:</span>
-        <select
-          className="px-2 py-1"
-          value={bookId}
-          onChange={(e) => {
-            setBookId(e.target.value as BookId);
+      <div className="flex flex-col items-center justify-center min-w-[80dvw]">
+        {/* <p className="text-3xl my-2">CROWN & 速単 音声プレイヤー</p> */}
+        <p className="text-3xl my-2">英語教材 音声プレイヤー</p>
+        <label className="text-lg my-2 w-full">
+          <span className="mr-2">教材を選択</span>
+          <select
+            className="px-2 py-1"
+            value={bookId}
+            onChange={(e) => {
+              setBookId(e.target.value as BookId);
 
-            // reset custom repeat value
-            if (customRepeatStart < 1) {
-              setCustomRepeatStart(1);
-            }
-            const newMax = GetBook(bookId).sections.length;
-            if (customRepeatEnd > newMax) {
-              const newEnd = newMax;
-              setCustomRepeatEnd(newEnd);
-            }
+              // reset custom repeat value
+              if (customRepeatStart < 1) {
+                setCustomRepeatStart(1);
+              }
+              const newMax = GetBook(bookId).sections.length;
+              if (customRepeatEnd > newMax) {
+                const newEnd = newMax;
+                setCustomRepeatEnd(newEnd);
+              }
 
-            //一応音声と番号リセット
-            setAudioId(0);
-            setTrackNo(0);
-          }}>
-          <optgroup label="速読英単語">
-            {sokutan.map((book) => (
-              <option key={book.title} value={book.id}>
-                {book.title}
+              //一応音声と番号リセット
+              setAudioId(0);
+              setTrackNo(0);
+            }}>
+            <optgroup label="速読英単語">
+              {sokutan.map((book) => (
+                <option key={book.title} value={book.id}>
+                  {book.title}
+                </option>
+              ))}
+            </optgroup>
+            {/* <optgroup label="CROWN 令和4年度～6年度版"></optgroup> */}
+            {/* <optgroup label="CROWN 令和8年度～版"></optgroup> */}
+          </select>
+        </label>
+        <label className="text-lg my-2 w-full">
+          <span className="mr-2">音声を選択</span>
+          <select className="px-2 py-1" value={audioId} onChange={(e) => setAudioId(Number(e.target.value))}>
+            {GetBook(bookId).audios.map((audio, index) => (
+              <option key={audio.name} value={index}>
+                {audio.name}
               </option>
             ))}
-          </optgroup>
-          {/* <optgroup label="CROWN 令和4年度～6年度版"></optgroup> */}
-          {/* <optgroup label="CROWN 令和8年度～版"></optgroup> */}
-        </select>
-      </label>
-      <label className="text-lg my-2">
-        <span className="mr-2">音声のタイプを選択:</span>
-        <select className="px-2 py-1" value={audioId} onChange={(e) => setAudioId(Number(e.target.value))}>
-          {GetBook(bookId).audios.map((audio, index) => (
-            <option key={audio.name} value={index}>
-              {audio.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      {/* a lot of thanks for @tau34 */}
-      {/* {trackNo === 0 && (
+          </select>
+        </label>
+        {/* a lot of thanks for @tau34 */}
+        {/* {trackNo === 0 && (
         <>
           <p className="text-lg mb-4">トラック番号を選択:</p>
           <div className="grid grid-cols-6 gap-2 mb-4">
@@ -254,32 +255,33 @@ function App() {
           </div>
         </>
       )}*/}
-      {trackNo > 0 && (
-        <>
-          <p className="text-lg mb-2">
-            No.{trackNo} 「{GetBook(bookId).sections[trackNo - 1]}」
-          </p>
-          {/* <button className="mb-4  px-4 py-1" onClick={() => setTrackNo(0)}>
+        {trackNo > 0 && (
+          <>
+            <p className="text-lg mb-2">
+              No.{trackNo} 「{GetBook(bookId).sections[trackNo - 1]}」
+            </p>
+            {/* <button className="mb-4  px-4 py-1" onClick={() => setTrackNo(0)}>
             トラック番号選択に戻る
           </button> */}
-        </>
-      )}
+          </>
+        )}
+      </div>
       <div className="flex flex-row my-2">
         <div className="mx-1">
           <button onClick={() => setIsModalOpened(!isModalOpened)}>トラックを選択</button>
           {isModalOpened && (
             <>
               <div className="absolute bg-[#00000096] w-full h-full z-700 left-0 right-0 top-0 bottom-0 m-auto" onClick={() => setIsModalOpened(false)}></div>
-              <div className="absolute bg-white border border-gray-200 rounded-md w-[80%] h-[80%] z-800 top-0 left-0 bottom-0 right-0 m-auto shadow-xl backdrop-blur-xl overflow-y-scroll p-8">
+              <div className="absolute bg-white border border-gray-200 rounded-md w-[85%] h-[85%] z-800 top-0 left-0 bottom-0 right-0 m-auto shadow-xl backdrop-blur-xl overflow-y-scroll p-8">
                 <div className="flex justify-center flex-col items-center">
                   <p className="text-xl mb-2">再生するトラックを選択</p>
                   <div className="flex items-center flex-col">
                     {Array.from({ length: GetBook(bookId).sections.length }, (_, i) => (
                       <div className="flex flex-row w-full items-center" key={i}>
-                        {trackNo == i + 1 ? <FaPlay /> : <span className="mr-1">{i + 1}.</span>}
+                        {trackNo == i + 1 ? <FaPlay className="min-w-[8%] text-center" /> : <span className="min-w-[8%] text-center">{i + 1}.</span>}
                         <button
                           key={i}
-                          className={`px-4 py-2 m-2`}
+                          className="px-4 py-2 m-2 min-w-[90%]"
                           onClick={() => {
                             setIsModalOpened(false);
                             setTrackNo(i + 1);
@@ -360,8 +362,8 @@ function App() {
             秒
           </label> */}
           <p className="text-lg my-2">
-            再生速度:
             <label>
+              再生速度
               <input
                 type="number"
                 min={SPEED_MIN}
@@ -401,7 +403,7 @@ function App() {
             ))}
           </div>
           <label className="text-lg my-2">
-            <span className="mr-2">リピート:</span>
+            <span className="mr-2">リピート</span>
             <select
               className="px-2 py-1"
               value={repeatMode}
@@ -418,7 +420,7 @@ function App() {
           <>
             <label className="text-lg my-2">
               {/* <span className="mr-2">開始:</span> */}
-              <span className="mr-2">範囲指定:</span>
+              <span className="mr-2">範囲指定</span>
               <input
                 disabled={repeatMode != 'custom'}
                 type="number"
@@ -474,7 +476,7 @@ function App() {
         </div>
       </details>
 
-      <div className="my-2">
+      <div className="m-2">
         <a href="https://www.zkai.co.jp/" target="_blank" referrerPolicy="no-referrer">
           <p>このウェブサイトは、非営利及び教育目的で作成されました。</p>
           <p>全ての音声データの権利はZ会に帰属します。</p>
