06a9dc36096cba45c2b8117f7527bbc38fa83192
Shota-Sunada <shota.sunada.dev@gmail.com>
2026-07-21 22:11:06 +0900

    update some system to add new book easily

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,4 +1,5 @@
 export type Book = {
+  id: string;
   title: string;
   audios: AudioSource[];
   sections: string[];
@@ -12,10 +13,12 @@ export type AudioSource = {
 export type RepeatMode = 'off' | 'one' | 'all' | 'custom';
 
 export type TrackInfo = {
-  bookId: number;
+  bookId: BookId | undefined;
   audioId: number;
   trackNo: number;
   bookName: string;
   audioName: string;
   trackName: string;
 };
+
+export type BookId = 'sokutan_nyumon' | 'sokutan_hisshu' | 'sokutan_jokyu';
